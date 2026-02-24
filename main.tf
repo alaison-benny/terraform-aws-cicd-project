f5d86f45a08bb85ae7c391fe5acbe8eee5933cec
alaison-benny <alisaalaison@gmail.com>
2026-02-24 11:08:08 +0530

    ok

--- main.tf
+++ main.tf
@@ -11,22 +11,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-# --- Backend Configuration ---
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "alaison-terraform-state-2026"
-  force_destroy = false
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 # --- VPC & Networking ---
 resource "aws_vpc" "day3_vpc" {
   cidr_block           = "10.0.0.0/16"
